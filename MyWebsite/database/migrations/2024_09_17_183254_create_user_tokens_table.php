cf306bb304d1b3e723c3d450275f36e890990cb5
Mahdi_Khesali <khesali.m.mahdi@gmail.com>
2024-09-21 18:57:20 +0330

    #5: design users_tokens table

--- MyWebsite/database/migrations/2024_09_17_183254_create_user_tokens_table.php
+++ MyWebsite/database/migrations/2024_09_17_183254_create_user_tokens_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('user_tokens', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->integer('tokenable_id');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('user_tokens');
     }
 };

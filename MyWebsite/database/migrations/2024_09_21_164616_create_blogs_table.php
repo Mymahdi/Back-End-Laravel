69320e64f7af5b8094b852e70a8066287b1f8dcc
Mahdi_Khesali <khesali.m.mahdi@gmail.com>
2024-09-21 20:21:52 +0330

    #6: set a table for blogs

---
+++ MyWebsite/database/migrations/2024_09_21_164616_create_blogs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->string('author_name');
+            
+            $table->unsignedBigInteger('user_id')->nullable();
+            
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('last_update')->nullable();
+
+            $table->integer('num_likes')->default(0); 
+            $table->integer('num_tags')->default(0);  
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}

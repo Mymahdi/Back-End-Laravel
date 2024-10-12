aba9b898850c308bf600295dcfdfb9a941df0fc2
Mahdi_Khesali <khesali.m.mahdi@gmail.com>
2024-10-12 18:48:58 +0330

    #40 Feature: remove the parrent_id in table

--- MyWebsite/database/migrations/2024_10_02_193643_create_comments_table.php
+++ MyWebsite/database/migrations/2024_10_02_193643_create_comments_table.php
@@ -12,7 +12,6 @@ class CreateCommentsTable extends Migration
             $table->id();
             $table->text('body');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->unsignedBigInteger('parent_id')->nullable();
             $table->morphs('commentable');
             $table->timestamp('created_at');
         });
